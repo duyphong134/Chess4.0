a430cf3745f130795a92cb765f3b3d33493ac373
NguyenVanThanhTung <160724289+NguyenVanThanhTung@users.noreply.github.com>
2024-05-07 22:46:47 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,20 +29,7 @@ int main(int argc, char *argv[]){
     Pos mouse;
     Pos mouseClick;
     Pieces piece;
-//    for(int i=0; i<64; i++){
-//        std::cout << game.pos_v1[game.move_count][i] << " ";
-//        if(i %8==7){
-//            std::cout << std::endl;
-//        }
-//    }
-//    std::cout << std::endl;
-//    for(int i=0; i<8; i++){
-//        for(int j=0; j<8; j++){
-//            std::cout << game.pos_v2[i][j] << " ";
-//        }
-//        std::cout << std::endl;
-//    }
-//    std::cout << std::endl;
+
     while(game.running == true){
         frame_start = SDL_GetTicks();
 
@@ -50,7 +37,7 @@ int main(int argc, char *argv[]){
 
         update(game, screen, mouseClick, piece);
 
-        render(game, screen, mouse);
+        render(game, screen, mouse, piece);
 
         frame_time = SDL_GetTicks() - frame_start;
         if(FRAME_DELAY >frame_time){
@@ -59,5 +46,5 @@ int main(int argc, char *argv[]){
     }
     game.quit();
     screen.quit();
-//    return 0;
+    return 0;
 }
